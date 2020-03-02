f2585bfa2f63d218a0b89204e4314db7dc90e5bf
Nathan <skeks463@gmail.com>
2020-03-02 11:36:05 +0900

    Add item5

---
+++ src/chapter2/item5/di/Dog.java
@@ -0,0 +1,9 @@
+package chapter2.item5.di;
+
+public class Dog {
+    private final Dress dress;
+
+    public Dog(Dress dress) {
+        this.dress = dress;
+    }
+}
---
+++ src/chapter2/item5/di/Dress.java
@@ -0,0 +1,7 @@
+package chapter2.item5.di;
+
+public class Dress {
+    public Dress() {
+        System.out.println("멍멍이에게 새로운 옷을 입혀준다.");
+    }
+}
